a29eae0d25c348a72495318f619d2560097b36e1
PRONOB BISWAS <biswaspronob@outlook.com>
2024-01-19 04:54:25 +0600

    update

---
+++ js/sccript.js
@@ -0,0 +1,33 @@
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// const numbers = [45, 4, 9, 16, 25];
+
+// let i = 0;
+// let len = cars.length;
+// let text = "";
+
+// for (; i < len; ) {
+//   text += cars + "<br>";
+//   i++;
+// }
+
+
+
+// let txt = "";
+// for (let x in numbers) {
+//   txt += numbers[x] + "<br>"; 
+// }
+
+// document.getElementById("headding").innerHTML = text;
+// document.getElementById("demo").innerHTML = txt;
+// console.log(text);
+// console.log(txt);
+// const numbers = [45, 4, 9, 16, 25];
+
+// let txt = "";
+// numbers.forEach(myFunction);
+// document.getElementById("demo").innerHTML = txt;
+
+// function myFunction(value, index, array) {
+//   txt += value + "<br>"; 
+// }
+// console.log(txt);
