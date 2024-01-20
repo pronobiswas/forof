d449d1da1d5cf6fc668001edff717e65c349476f
PRONOB BISWAS <biswaspronob@outlook.com>
2024-01-21 04:15:28 +0600

    after so much trying I sucessful output an namta

--- js/sccript.js
+++ js/sccript.js
@@ -31,11 +31,19 @@
 //   txt += value + "<br>"; 
 // }
 // console.log(txt);
-const fruits = ["banana", "orange", "Apple", "mango"];
+// const fruits = ["banana", "orange", "Apple", "mango"];
 
-const summerFruits = [];
-summerFruits[0] = ["apple", "banana", "lici"];
-summerFruits[1] = {payara:"green" , papaya:4};
-summerFruits[3] = "amader fal";
-let x = summerFruits.flat();
-console.log(x);
+// const summerFruits = [];
+// summerFruits[0] = ["apple", "banana", "lici"];
+// summerFruits[1] = {payara:"green" , papaya:4};
+// summerFruits[3] = "amader fal";
+// let x = summerFruits.flat();
+let namta = document.getElementById("namta");
+let nums = [1,2,3,4,5,6,7,8,9,10];
+let num = ""
+
+for(let i = 0; i < 11; i++){
+    num +=5 + "x" + nums[i] + "=" + nums[i]*5 + "<br>";
+};
+
+namta.innerHTML = num;
